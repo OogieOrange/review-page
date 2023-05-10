057600600cf4738c09af4b6c927b84b766d74cc3
OogieOrange <>
2023-05-10 14:15:49 +0000

    Create ProductPosts view

--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+class ProductPosts(generic.ListView):
+    model = Product
+    queryset = Product.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 12

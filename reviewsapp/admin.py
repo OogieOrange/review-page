8d6a7f025eb9fc4a1a8e6e517dccb432284e93ff
OogieOrange <>
2023-05-10 13:47:48 +0000

    Connect comment model to admin and add navigational tools

--- reviewsapp/admin.py
+++ reviewsapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Product)
-class PostAdmin(SummernoteModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on', 'title')
@@ -13,3 +13,9 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('description')
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('name', 'created_on')
+    list_display = ('name', 'post', 'created_on')
+    search_fields = ['name', 'created_on', 'body']

096c785213faea58093946cca187c2831cd94b5e
OogieOrange <>
2023-05-10 16:07:59 +0000

    Add and style base.html

--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Product
 
+
 class ProductPosts(generic.ListView):
     model = Product
     queryset = Product.objects.filter(status=1).order_by('created_on')

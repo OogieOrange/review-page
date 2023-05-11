f134f63fc6ae9ddcba9e74c16c23f098adc5e4a9
OogieOrange <>
2023-05-11 14:46:28 +0000

    Edit base.html and index.html

--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Product
 
 
@@ -9,3 +9,4 @@ class ProductPosts(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'product_posts'
     paginate_by = 12
+

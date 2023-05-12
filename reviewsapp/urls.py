d2a032eeef5f31f2aa9a086bb8c8da0c1af8028c
OogieOrange <>
2023-05-12 10:35:27 +0000

    Add basic structure and connect ProductDetails view

--- reviewsapp/urls.py
+++ reviewsapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductPosts.as_view(), name='index')
+    path('', views.ProductPosts.as_view(), name='index'),
+    path('<slug:slug>/', views.ProductDetails.as_view(), name='product_detail'),
 ]
--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -9,3 +9,18 @@ class ProductPosts(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'product_posts'
     paginate_by = 12
+
+
+class ProductDetails(generic.ListView):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Product.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "review_details.html",
+            {
+                "product": post,
+            },
+        )

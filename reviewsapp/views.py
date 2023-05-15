de2ce5eab9c7df70f41fdff4649eae9feb1b780c
OogieOrange <>
2023-05-15 10:00:44 +0000

    Add base for comments

--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Product
+from .forms import CommentForm
 
 
 class ProductPosts(generic.ListView):
@@ -16,11 +17,14 @@ class ProductDetails(generic.ListView):
     def get(self, request, slug, *args, **kwargs):
         queryset = Product.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("created_on")
 
         return render(
             request,
             "review-details.html",
             {
                 "product": post,
+                "comments": comments,
+                "comment_form": CommentForm()
             },
         )
